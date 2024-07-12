fc3f9a95373143b73ccab801a7efa6856f8fd21c
kp3073 <keyurpatel8288@gmail.com>
2024-07-12 14:40:53 -0400

    1

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_ssm_parameter" "parameter" {
 
 variable "parameter" {
 default = [
-    {name="prod.rds.master_username",value = "expense", type = "String"},
+    {name="prod.rds.master_username",value = "expenseprod", type = "String"},
     {name="prod.rds.master_password",value = "expenseprod1234", type = "SecureString"},
    # {name="expense.frontend.backend_url",value = "internal-dev-private-757772481.us-east-1.elb.amazonaws.com", type = "String"},
     {name="prod.rds.endpoint",value = "aurora-cluster-prod.cluster-cjmks6wgq3k7.us-east-1.rds.amazonaws.com",type = "String"}]
