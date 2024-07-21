a0a616bed95522146ab992ea952bc884e0d4ee8e
kp3073 <keyurpatel8288@gmail.com>
2024-07-21 16:19:10 -0400

    11

--- main.tf
+++ main.tf
@@ -14,6 +14,7 @@ default = [
     {name="prod.rds.endpoint",value = "aurora-cluster-prod.cluster-cjmks6wgq3k7.us-east-1.rds.amazonaws.com",type = "String"},
   #dev environment
     {name="dev.expense.frontend.backend_url",value = "http://backend-dev.aligntune.online/", type = "String"},
+    ##{name="dev.rds.endpoint",value = "aurora-cluster-dev.cluster-cjmks6wgq3k7.us-east-1.rds.amazonaws.com",type = "String"},
   #common env
    {name="jenkins_password",value = "Canada1991$", type = "SecureString"}
   ]
