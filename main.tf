7d66ec61f2ac8b5ff9e6160449b3c7abb03b2309
kp3073 <keyurpatel8288@gmail.com>
2024-08-31 17:15:17 -0400

    12

--- main.tf
+++ main.tf
@@ -16,12 +16,14 @@ variable "parameter" {
       name = "prod.rds.endpoint", value = "aurora-cluster-prod.cluster-cjmks6wgq3k7.us-east-1.rds.amazonaws.com",
       type = "String"
     },
-    { name = "prod.frontend.app_version", value = "1.0.2", type = "String" },
+    { name = "prod.frontend.app_version", value = "1.0.8", type = "String" },
     { name = "prod.backend.app_version", value = "1.0.2", type = "String" },
     #dev environment
     { name = "dev.expense.frontend.backend_url", value = "http://backend-dev.aligntune.online/", type = "String" },
     ##{name="dev.rds.endpoint",value = "aurora-cluster-dev.cluster-cjmks6wgq3k7.us-east-1.rds.amazonaws.com",type = "String"},
     #common env
     { name = "jenkins_password", value = "Canada1991$", type = "SecureString" }
+    { name = "artifactory_username", value = "admin", type = "String" }
+    { name = "artifactory_password", value = "Canada1991$", type = "SecureString" }
   ]
 }
